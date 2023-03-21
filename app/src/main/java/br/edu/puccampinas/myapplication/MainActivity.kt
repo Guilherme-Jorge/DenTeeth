17869cc1b7e7ff635dd961fab261b09c591c1971
Guilherme Jorge <guilherme.jorge010@gmail.com>
2023-03-21 16:00:19 -0300

    Adcionei arquivos básicos de um projeto Android

---
+++ app/src/main/java/br/edu/puccampinas/myapplication/MainActivity.kt
@@ -0,0 +1,11 @@
+package br.edu.puccampinas.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
