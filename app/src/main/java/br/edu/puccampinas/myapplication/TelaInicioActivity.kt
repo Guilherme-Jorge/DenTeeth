f98ce24a2026dfee8b2a3261046043b8dd57b374
Goldenn <joaopauloarrigo@gmail.com>
2023-04-02 16:34:46 -0300

    create: evento do botao registrar da tela de inicio

--- app/src/main/java/br/edu/puccampinas/myapplication/TelaInicioActivity.kt
+++ app/src/main/java/br/edu/puccampinas/myapplication/TelaInicioActivity.kt
@@ -1,11 +1,25 @@
 package br.edu.puccampinas.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.AppCompatTextView
 
 class TelaInicioActivity : AppCompatActivity() {
+
+    private lateinit var tvRegistrar : AppCompatTextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tela_inicio)
+
+        //Inicializaçâo do Intent para a tela de Registro
+        //O Intent está redirecionando para si mesmo, deve ser trocado para a tela de registrar
+        var intentTelaRegistro = Intent(this, TelaInicioActivity::class.java)
+
+        tvRegistrar = findViewById(R.id.tvRegistrar)
+
+        tvRegistrar.setOnClickListener {
+            this.startActivity(intentTelaRegistro)
+        }
     }
 }
