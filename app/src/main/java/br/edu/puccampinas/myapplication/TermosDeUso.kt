c23136e1e64614985163fd02f29a5cf0e01f63b8
Bruno <contagames00@gmail.com>
2023-04-02 15:56:18 -0300

    Criei a tela de Termos De Uso.

---
+++ app/src/main/java/br/edu/puccampinas/myapplication/TermosDeUso.kt
@@ -0,0 +1,11 @@
+package br.edu.puccampinas.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TermosDeUso : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_termosdeuso)
+    }
+}
