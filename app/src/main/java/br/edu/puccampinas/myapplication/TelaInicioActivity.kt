1b8e2d6ffe442a7e5e591f3d9244a5a0d80468a4
Guilherme Jorge <guilherme.jorge010@gmail.com>
2023-04-04 15:42:17 -0300

    add: implementei o firebase functions nas telas de criar conta, além de conectá-las com intents

--- app/src/main/java/br/edu/puccampinas/myapplication/TelaInicioActivity.kt
+++ app/src/main/java/br/edu/puccampinas/myapplication/TelaInicioActivity.kt
@@ -3,23 +3,32 @@ package br.edu.puccampinas.myapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.widget.AppCompatTextView
+import br.edu.puccampinas.myapplication.databinding.ActivityTelaInicioBinding
 
 class TelaInicioActivity : AppCompatActivity() {
 
-    private lateinit var tvRegistrar : AppCompatTextView
+    private lateinit var binding: ActivityTelaInicioBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_tela_inicio)
+        binding = ActivityTelaInicioBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         //Inicializaçâo do Intent para a tela de Registro
-        //O Intent está redirecionando para si mesmo, deve ser trocado para a tela de registrar
-        var intentTelaRegistro = Intent(this, TelaInicioActivity::class.java)
+        val intentTelaRegistro = Intent(this, RegistroActivity::class.java)
 
-        tvRegistrar = findViewById(R.id.tvRegistrar)
+        binding.tvRegistrar.setOnClickListener {
+            hideSoftKeyboard(binding.tvRegistrar)
 
-        tvRegistrar.setOnClickListener {
             this.startActivity(intentTelaRegistro)
         }
+    }
+
+    private fun hideSoftKeyboard(v: View) {
+
+        val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(v.windowToken, 0)
     }
 }
--- app/src/main/java/br/edu/puccampinas/myapplication/TermosDeUsoActivity.kt
+++ app/src/main/java/br/edu/puccampinas/myapplication/TermosDeUsoActivity.kt
@@ -2,10 +2,47 @@ package br.edu.puccampinas.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import br.edu.puccampinas.myapplication.databinding.ActivityTermosdeusoBinding
+import com.google.firebase.functions.FirebaseFunctions
+import com.google.firebase.functions.FirebaseFunctionsException
+import com.google.android.gms.tasks.Task
 
 class TermosDeUsoActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityTermosdeusoBinding
+    private val TAG = "MainActivity Firebase"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_termosdeuso)
+        binding = ActivityTermosdeusoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnAceitarTermos.setOnClickListener {
+            adcionarProfissional()
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        Log.d(TAG, "Usuário salvo com sucesso: ${task.result}")
+                    } else {
+                        val e = task.exception
+                        if (e is FirebaseFunctionsException) {
+                            Log.e(TAG, "Erro ao salvar usuário: [${e.code}] ${e.details}")
+                        } else {
+                            Log.w(TAG, "Erro desconhecido: ${task.exception}")
+                        }
+                    }
+                }
+        }
+    }
+
+    private fun adcionarProfissional(): Task<String> {
+        return FirebaseFunctions
+            .getInstance("southamerica-east1")
+            .getHttpsCallable("salvarDadosPessoais")
+            .call(intent.data)
+            .continueWith { task ->
+                val result = task.result?.data as String
+                result
+            }
     }
 }
