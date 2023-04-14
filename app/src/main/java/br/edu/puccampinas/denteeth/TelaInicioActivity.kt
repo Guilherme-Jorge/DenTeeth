da1339a27774f82e61ec4771af3bc6f07df5335c
Guilherme Jorge <guilherme.jorge010@gmail.com>
2023-04-13 22:34:17 -0300

    add: implementei uma checagem de campo para o login do usuário

--- app/src/main/java/br/edu/puccampinas/denteeth/TelaInicioActivity.kt
+++ app/src/main/java/br/edu/puccampinas/denteeth/TelaInicioActivity.kt
@@ -3,10 +3,14 @@ package br.edu.puccampinas.denteeth
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.widget.AppCompatTextView
+import androidx.core.view.isEmpty
 import br.edu.puccampinas.denteeth.databinding.ActivityTelaInicioBinding
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputLayout
 
 class TelaInicioActivity : AppCompatActivity() {
 
@@ -19,8 +23,22 @@ class TelaInicioActivity : AppCompatActivity() {
         //Inicializaçâo do Intent para a tela de Registro
         val intentTelaRegistro = Intent(this, RegistroActivity::class.java)
 
-        binding.tvRegistrar.setOnClickListener {
-            hideSoftKeyboard(binding.tvRegistrar)
+        binding.btnEntrar.setOnClickListener {
+            hideSoftKeyboard(binding.btnEntrar)
+
+            if (binding.etEmail.text?.isEmpty() == true) {
+                Snackbar.make(binding.root, "Campo de E-mail vazio.", Snackbar.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+
+            if (binding.etSenha.text?.isEmpty() == true) {
+                Snackbar.make(binding.root, "Campo de Senha vazio.", Snackbar.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+        }
+
+        binding.btnRegistrar.setOnClickListener {
+            hideSoftKeyboard(binding.btnRegistrar)
 
             this.startActivity(intentTelaRegistro)
         }
@@ -31,4 +49,10 @@ class TelaInicioActivity : AppCompatActivity() {
         val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(v.windowToken, 0)
     }
+
+//    private fun checkEmptyTextInput(input: TextInputLayout) {
+//        if (input.isEmpty()) {
+//            Snackbar.make(binding.root, "Campo de ${input.labelFor} vazio.", Snackbar.LENGTH_LONG).show()
+//        } else {}
+//    }
 }
