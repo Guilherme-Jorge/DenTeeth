a167cb5b15fa921306007213d67a3dd9bd5050d2
Guilherme Jorge <guilherme.jorge010@gmail.com>
2023-05-23 19:20:13 -0300

    feat: implementei elementos clicaveis na lista de emergência

--- app/src/main/java/br/edu/puccampinas/denteeth/classes/Emergencia.kt
+++ app/src/main/java/br/edu/puccampinas/denteeth/classes/Emergencia.kt
@@ -1,3 +1,3 @@
 package br.edu.puccampinas.denteeth.classes
 
-data class Emergencia(val dataHora: Any?, val descricao: Any?, val fotos: Any?, val nome: Any?, val status: Any?, val telefone: Any?)
+data class Emergencia(val dataHora: Any?, val descricao: Any?, val fotos: Any?, val id: Any?, val nome: Any?, val status: Any?, val telefone: Any?)
