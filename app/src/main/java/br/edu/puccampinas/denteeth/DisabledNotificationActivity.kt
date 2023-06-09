b6d2bf0119a0d5113535d478ec470b7929d1f41e
BrunoAlves10 <contagames00@gmail.com>
2023-06-09 19:19:45 -0300

    feat: Criei a tela de notificação desligada, mudei o termos de uso e adicionei novos campos de endereço

--- app/src/main/java/br/edu/puccampinas/denteeth/DisabledNotificationActivity.kt
+++ app/src/main/java/br/edu/puccampinas/denteeth/DisabledNotificationActivity.kt
@@ -1,11 +1,23 @@
 package br.edu.puccampinas.denteeth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.view.View
+import com.google.android.material.button.MaterialButton
 
 class DisabledNotificationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_disabled_notification)
+
+        val btnAbrirConfig: MaterialButton = findViewById(R.id.btnAbrirConfig)
+        btnAbrirConfig.setOnClickListener {
+            val intent = Intent(Settings.ACTION_APP_NOTIFICATION_SETTINGS)
+            intent.putExtra(Settings.EXTRA_APP_PACKAGE, packageName)
+            startActivity(intent)
+        }
+
     }
 }
