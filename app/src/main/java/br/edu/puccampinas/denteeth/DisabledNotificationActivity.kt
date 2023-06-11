ae129965e1981b082065012fe379d77c4a6d3d35
Guilherme Jorge <guilherme.jorge010@gmail.com>
2023-06-11 12:59:44 -0300

    fix: arrumei problema com o layout do registro, implementei o botão de voltar na tela de ativar notificação e reordenei os códigos

--- app/src/main/java/br/edu/puccampinas/denteeth/DisabledNotificationActivity.kt
+++ app/src/main/java/br/edu/puccampinas/denteeth/DisabledNotificationActivity.kt
@@ -1,23 +1,30 @@
 package br.edu.puccampinas.denteeth
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.Settings
-import android.view.View
-import com.google.android.material.button.MaterialButton
+import androidx.appcompat.app.AppCompatActivity
+import br.edu.puccampinas.denteeth.databinding.ActivityDisabledNotificationBinding
 
 class DisabledNotificationActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityDisabledNotificationBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_disabled_notification)
 
-        val btnAbrirConfig: MaterialButton = findViewById(R.id.btnAbrirConfig)
-        btnAbrirConfig.setOnClickListener {
-            val intent = Intent(Settings.ACTION_APP_NOTIFICATION_SETTINGS)
-            intent.putExtra(Settings.EXTRA_APP_PACKAGE, packageName)
-            startActivity(intent)
+        binding = ActivityDisabledNotificationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnAbrirConfig.setOnClickListener {
+            val intentNotification = Intent(Settings.ACTION_APP_NOTIFICATION_SETTINGS)
+            intentNotification.putExtra(Settings.EXTRA_APP_PACKAGE, packageName)
+            startActivity(intentNotification)
         }
 
+        binding.btnVoltar.setOnClickListener {
+            val intentTelaInicio = Intent(this, TelaInicioActivity::class.java)
+            startActivity(intentTelaInicio)
+        }
     }
 }
