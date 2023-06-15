52ecf985bc63be8bdebe84557ce2954512ec45e7
Guilherme Jorge <guilherme.jorge010@gmail.com>
2023-06-14 22:45:18 -0300

    fix: ajustei o serviço de notificações para incluir a de telefone e mapa

--- app/src/main/java/br/edu/puccampinas/denteeth/messaging/DefaultMessageService.kt
+++ app/src/main/java/br/edu/puccampinas/denteeth/messaging/DefaultMessageService.kt
@@ -13,7 +13,7 @@ import br.edu.puccampinas.denteeth.classes.CustomResponse
 import br.edu.puccampinas.denteeth.R
 import br.edu.puccampinas.denteeth.datastore.UserPreferencesRepository
 import br.edu.puccampinas.denteeth.emergencia.AtenderEmergenciaActivity
-import br.edu.puccampinas.denteeth.emergencia.EmergenciaFragment
+import br.edu.puccampinas.denteeth.emergencia.AtendimentoActivity
 import com.google.android.gms.tasks.Task
 import com.google.firebase.functions.FirebaseFunctions
 import com.google.firebase.functions.ktx.functions
@@ -31,12 +31,17 @@ class DefaultMessageService : FirebaseMessagingService() {
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
         val msgData = remoteMessage.data
         Log.d("Teste", msgData.toString())
-        if (msgData["descricao"] != null) {
-            val msg = msgData["descricao"]
-            showNotificationEmergencia(msg!!, msgData)
+        if (msgData["type"].toString() == "novaEmergencia") {
+            val msg = msgData["descricao"].toString()
+            showNotificationEmergencia(msg, msgData)
+        } else if (msgData["type"].toString() == "mapa") {
+            val msg = msgData["texto"].toString()
+            showNotificationMapa(msg, msgData)
+        } else if (msgData["type"].toString() == "telefone") {
+            val msg = msgData["texto"].toString()
+            showNotificationTelefone(msg, msgData)
         } else {
-            val msg = msgData["texto"]
-            showNotificationResposta(msg!!)
+            //
         }
     }
 
@@ -79,18 +84,15 @@ class DefaultMessageService : FirebaseMessagingService() {
     }
 
     private fun showNotificationEmergencia(messageBody: String, messageData: Map<String, String>) {
-        val fotos = messageData["fotos"] as List<String>
         val intent = Intent(this, AtenderEmergenciaActivity::class.java)
         intent.action = "actionstring" + System.currentTimeMillis()
-        intent.putExtra("nome", messageData["nome"])
-        intent.putExtra("telefone", messageData["telefone"])
-        intent.putExtra("fotos1", fotos[0])
-        intent.putExtra("fotos2", fotos[1])
-        intent.putExtra("fotos3", fotos[2])
-        intent.putExtra("status", messageData["status"])
-        intent.putExtra("descricao", messageData["descricao"])
-        intent.putExtra("dataHora", messageData["dataHora"])
-        intent.putExtra("id", messageData["id"])
+        intent.putExtra("nome", messageData["nome"].toString())
+        intent.putExtra("fotos1", messageData["fotos1"].toString())
+        intent.putExtra("fotos2", messageData["fotos2"].toString())
+        intent.putExtra("fotos3", messageData["fotos3"].toString())
+        intent.putExtra("status", messageData["status"].toString())
+        intent.putExtra("descricao", messageData["descricao"].toString())
+        intent.putExtra("id", messageData["id"].toString())
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
         val pendingIntent = PendingIntent.getActivity(
             this, 0 /* Request code */, intent,
@@ -117,8 +119,12 @@ class DefaultMessageService : FirebaseMessagingService() {
         notificationManager.notify(0 /* ID of notification */, notificationBuilder.build())
     }
 
-    private fun showNotificationResposta(messageBody: String) {
-        val intent = Intent(this, EmergenciaFragment::class.java)
+    private fun showNotificationTelefone(messageBody: String, messageData: Map<String, String>) {
+        val intent = Intent(this, AtendimentoActivity::class.java)
+        intent.action = "actionstring" + System.currentTimeMillis()
+        intent.putExtra("telefone", messageData["telefone"].toString())
+        intent.putExtra("fcmToken", messageData["fcmToken"].toString())
+        intent.putExtra("nome", messageData["nome"].toString())
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
         val pendingIntent = PendingIntent.getActivity(
             this, 0 /* Request code */, intent,
@@ -129,6 +135,40 @@ class DefaultMessageService : FirebaseMessagingService() {
         val notificationBuilder = NotificationCompat.Builder(this, channelId)
             .setSmallIcon(R.drawable.ic_tooth)
             .setContentTitle(getString(R.string.fcm_secondary_title_message))
+            .setContentText(messageBody)
+            .setColor(ContextCompat.getColor(this, R.color.blue_main))
+            .setAutoCancel(true)
+            .setSound(defaultSoundUri)
+            .setContentIntent(pendingIntent)
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val channel = NotificationChannel(
+            channelId,
+            "Channel human readable title",
+            NotificationManager.IMPORTANCE_DEFAULT
+        )
+        notificationManager.createNotificationChannel(channel)
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build())
+    }
+
+    private fun showNotificationMapa(messageBody: String, messageData: Map<String, String>) {
+        val intent = Intent(this, AtendimentoActivity::class.java)
+        intent.action = "actionstring" + System.currentTimeMillis()
+        intent.putExtra("lat", messageData["lat"].toString())
+        intent.putExtra("lng", messageData["lng"].toString())
+        intent.putExtra("titulo", messageData["titulo"].toString())
+        intent.putExtra("endereco", messageData["endereco"].toString())
+        intent.putExtra("fcmToken", messageData["fcmToken"].toString())
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val pendingIntent = PendingIntent.getActivity(
+            this, 0 /* Request code */, intent,
+            PendingIntent.FLAG_IMMUTABLE
+        )
+        val channelId = getString(R.string.default_notification_channel_id)
+        val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        val notificationBuilder = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.ic_tooth)
+            .setContentTitle(getString(R.string.fcm_tertiary_title_message))
             .setContentText(messageBody)
             .setColor(ContextCompat.getColor(this, R.color.blue_main))
             .setAutoCancel(true)
